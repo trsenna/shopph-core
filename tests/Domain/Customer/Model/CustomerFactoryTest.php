f43916ffde34e03cabbff95ad41adcc280d1d342
Thiago Senna <trsenna@gmail.com>
2021-09-25 16:00:59 -0300

    added customer factory test cases

---
+++ tests/Domain/Customer/Model/CustomerFactoryTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Shopph\Tests\Domain\Customer\Model;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Shopph\Contract\Foundation\Model\IdentityFactoryInterface;
+use Shopph\Domain\Customer\Model\CustomerFactory;
+
+use function Shopph\Tests\factory_identity;
+
+final class CustomerFactoryTest extends TestCase
+{
+    private ?IdentityFactoryInterface $identityFactory = null;
+
+    public function setUp(): void
+    {
+        /** @var MockObject $identityFactoryMock */
+        $identityFactoryMock = $this->createMock(IdentityFactoryInterface::class);
+        $identityFactoryMock->method('create')->willReturn(
+            factory_identity('87ffd646-9ef8-473b-951c-28f53fe8cadc')
+        );
+
+        $this->identityFactory = $identityFactoryMock;
+    }
+
+    public function testCreateWhenCalledMustReturnCustomerWithIdentity()
+    {
+        $customerFactory = new CustomerFactory($this->identityFactory);
+
+        $customer = $customerFactory->create('Jon');
+        $customerIdentity = $customer->getIdentity();
+
+        $this->assertNotNull($customer);
+        $this->assertNotNull($customerIdentity);
+        $this->assertEquals('87ffd646-9ef8-473b-951c-28f53fe8cadc', $customerIdentity->value());
+    }
+
+    public function testCreateWhenCalledMustReturnCustomerWithName()
+    {
+        $customerFactory = new CustomerFactory($this->identityFactory);
+
+        $customer = $customerFactory->create('Jon');
+        $customerName = $customer->getName();
+
+        $this->assertNotNull($customer);
+        $this->assertNotNull($customerName);
+        $this->assertEquals('Jon', $customerName->getFullName());
+    }
+}
