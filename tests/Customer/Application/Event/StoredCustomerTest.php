64f3243dd7685bc03a7f36359d21447a0c880bf8
Thiago Senna <trsenna@gmail.com>
2021-09-26 10:45:32 -0300

    added tests for application query response factories

--- tests/Customer/Application/Event/StoredCustomerTest.php
+++ tests/Customer/Application/Event/StoredCustomerTest.php
@@ -3,28 +3,28 @@
 namespace Shopph\Tests\Customer\Application\Event;
 
 use PHPUnit\Framework\TestCase;
+use Shopph\Customer\Application\Event\StoredCustomer;
 use Shopph\Customer\Domain\Model\Customer;
 use Shopph\Customer\Domain\Model\CustomerName;
-use Shopph\Foundation\Contract\Model\IdentityInterface;
 
 use function Shopph\Tests\factory_identity;
 
 final class StoredCustomerTest extends TestCase
 {
-    private ?IdentityInterface $identity;
-    private ?CustomerName $customerName;
+    private ?Customer $customer;
 
     public function setUp(): void
     {
-        $this->identity = factory_identity('87ffd646-9ef8-473b-951c-28f53fe8cadc');
-        $this->customerName = new CustomerName('Jon');
+        $this->customer = new Customer(
+            factory_identity('87ffd646-9ef8-473b-951c-28f53fe8cadc'),
+            new CustomerName('Jon')
+        );
     }
 
-    public function testFromObjectWhenCalledMustReturnTheEventInstance(): void
+    public function testFromEntityWhenCalledMustReturnValidInstance(): void
     {
-        $customer = new Customer($this->identity, $this->customerName);
-
-        $this->assertEquals('87ffd646-9ef8-473b-951c-28f53fe8cadc', $customer->getIdentity()->value());
-        $this->assertEquals('Jon', $customer->getName()->getFullName());
+        $event = StoredCustomer::fromEntity($this->customer);
+        $this->assertEquals('87ffd646-9ef8-473b-951c-28f53fe8cadc', $event->id);
+        $this->assertEquals('Jon', $event->name);
     }
 }
---
+++ tests/Customer/Application/Query/GetCustomersResponseTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Shopph\Tests\Customer\Application\Query;
+
+use PHPUnit\Framework\TestCase;
+use Shopph\Customer\Application\Query\GetCustomersResponse;
+
+final class GetCustomersResponseTest extends TestCase
+{
+    private ?array $customers;
+
+    public function setUp(): void
+    {
+        $this->customers[0] = new \stdClass;
+        $this->customers[0]->id = '87ffd646-9ef8-473b-951c-28f53fe8cadc';
+        $this->customers[0]->name = 'Jon';
+    }
+
+    public function testFromArrayWhenCalledMustReturnValidInstances(): void
+    {
+        $response = GetCustomersResponse::fromArray($this->customers);
+        $this->assertNotEmpty($response->customers);
+        $this->assertEquals('87ffd646-9ef8-473b-951c-28f53fe8cadc', $response->customers[0]->id);
+        $this->assertEquals('Jon', $response->customers[0]->name);
+    }
+}
